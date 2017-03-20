f58a4751a7c6a210a8aa9063ab8a22d3e84d63f2
Alex Clarke <a.clarke@runbox.com>
2017-03-20 13:16:19 +0000

    First Script Updates
    Page builder now generates table given a folder of images and the
    associated setup script

--- utility/page_builder.py
+++ utility/page_builder.py
@@ -1,24 +1,22 @@
-#Builds HTML table of flag images from a given JSON file and flag image folder
-#JSON file -> country_data.json
-#Flags folder -> /flags
-#Each row holds five cells, with a flag image in each cell
-#id is set to the 2-letter ISO country identifier
-#class is set to found/missing which determines image transparency using CSS
+#Given a folder containing a flag_list.txt file and associated images
+#Produce a HTML table with each cell holding a flag image
+#
+import sys;
+import os;
 
-import json
 
 #Vars
 cells_per_row = 19;
+directory = sys.argv[1]; #Second parameter is directory to process
+dirname = os.path.split(directory)[1]
+total = 0
 
-
-#Open the JSON file and convert it to a list of dictionariest
-#One dict for each country, with keys "Name" "Code"
-with open("country_data.json", mode='r', encoding='utf-8') as country_json_data:
-         country_data = json.load(country_json_data)
-
+print(directory)
+print(os.path.split(directory)[1])
+print("generated_pages/"+dirname+'.html')
 
 #Write to html file
-with open('FlagIt.html', mode="w+", encoding="utf-8") as html_page:
+with open("generated_pages/{0}.html".format(dirname), mode="w+", encoding="utf-8") as html_page:
 
     #Doc
     html_page.write("<!DOCTYPE html>\n")
@@ -31,11 +29,9 @@ with open('FlagIt.html', mode="w+", encoding="utf-8") as html_page:
     html_page.write("\t\t<script type=\"application/javascript\" src=\"browser-polyfill.js\"></script>\n")
     #Setup script - for handlers
     html_page.write("\t\t<script type=\"text/javascript\" src=\"FlagIt.js\"></script>\n\n")
-	html_page.write("\t</head>\n\n")
-
+    html_page.write("\t</head>\n\n")
     #Body start
     html_page.write("\t<body>\n\n")
-
     html_page.write("\t\t<table id=\"flag-table\">\n")
     #Cell counter, after 5 cells create a new row
     counter = 1
@@ -43,13 +39,14 @@ with open('FlagIt.html', mode="w+", encoding="utf-8") as html_page:
     total = 1
 
     #Generate table
-    for country in country_data:
+    for flag in os.listdir(directory):
+
+        filename = flag
+        countryname = os.path.splitext(flag)[0]
 
         #Check for apostrophes and escape
-        if "\'" in country["Name"]:
-            country["Name"] = country["Name"].replace("'", "\'") #Read up on escaping single quotes - tooltip not showing in FF
-
-
+        if "\'" in countryname:
+            countryname = countryname.replace("'", "\'") #Read up on escaping single quotes - tooltip not showing in FF
         if(counter == 1):
             html_page.write("\t\t\t<tr>\n")
 
@@ -57,18 +54,17 @@ with open('FlagIt.html', mode="w+", encoding="utf-8") as html_page:
 
         #Setup opening <td> and class / id
         #Look into stringbuilder equivalent for python
-        html_string  =  "\t\t\t\t\t<td class=\"missing\" id=\""
-        html_string +=  country["Code"]
-        html_string +=  "\">"
+        html_string  =  "\t\t\t\t\t<td class=\"missing\" id=\"{0}\">".format(countryname)
 
         #Inner img
         html_string += "<img id=\""
         html_string += str(total)
         html_string += "\" src=\"/flags/"
-        html_string += country["Code"]
-        html_string += ".png\" "
-        html_string += "title=\""
-        html_string += country["Name"]
+        html_string += dirname
+        html_string += "/"
+        html_string += filename
+        html_string += "\" title=\""
+        html_string += countryname
         html_string += "\"/>"
 
         #Close </td>
@@ -77,7 +73,7 @@ with open('FlagIt.html', mode="w+", encoding="utf-8") as html_page:
         #Write to file
         html_page.write(html_string)
 
-        if(counter > cells_per_row  or country == country_data[-1]):
+        if(counter > cells_per_row) or flag == os.listdir(directory)[-1]:
             html_page.write("\t\t\t</tr>\n")
             counter = 0
 
@@ -91,10 +87,15 @@ with open('FlagIt.html', mode="w+", encoding="utf-8") as html_page:
 
     #Setup script - initialises setup defined in FlagIt.js
 	#Will need to be generated alongside HTML
-    html_page.write("\t\t<script type=\"text/javascript\" src=\"WorldFlags.js\"></script>\n\n")
+    html_page.write("\t\t<script type=\"text/javascript\" src=\"{0}.js\"></script>\n\n".format(dirname))
 
     #Body end
     html_page.write("\t</body>\n\n")
 
     #Page end
     html_page.write("</html>")
+
+	#Write JS initialisation script
+	#Write to html file
+with open("generated_js/{0}.js".format(dirname), mode="w+", encoding="utf-8") as js_script:
+	js_script.write("init_setup(\"{0}\", {1})".format(dirname, total));
