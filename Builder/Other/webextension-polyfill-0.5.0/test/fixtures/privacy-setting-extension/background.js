33b29ada58bebbb7aa7d26e42c3c2cf350d0adce
Alex <a.clarke@runbox.com>
2019-10-21 20:09:17 +0100

    Removed polyfill repo

--- Builder/Other/webextension-polyfill-0.5.0/test/fixtures/privacy-setting-extension/background.js
+++
@@ -1,5 +0,0 @@
-browser.runtime.onMessage.addListener(async (msg) => {
-  let {method, args} = msg;
-  let result = await browser.privacy.services.passwordSavingEnabled[method](...args);
-  return {result, type: typeof result};
-});
